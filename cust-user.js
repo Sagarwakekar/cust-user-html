21bbdfb7cf7ca69b1fade2b0b2844ff1eabd5962
Sagar <wakekarsagar397@gmail.com>
2023-11-01 19:14:40 +0530

    implement js

--- cust-user.js
+++ cust-user.js
@@ -16,6 +16,35 @@ function submitForm() {
     console.log("Date of Birth: " + dateOfBirth);
     console.log("Address: " + address);
 
+    // document.getElementById('form2').innerHTML = `
+    //  <p>  1] NAME : ${name} </p>
+    
+    // `
+
+    document.getElementById('form2').innerHTML = `
+
+            <div>
+            1] NAME <br> <input type="text"  value=${name}>
+        </div>
+        <div>
+            2] E-MAIL <br> <input type="text" id="email" value=${email}>
+        </div>
+        <div>
+            3] MOB.NO <br> <input type="number" id="mob-no" value=${mobNo}>
+        </div>
+        <div>
+            4] D.O.B <br> <input type="date" id="date" value=${dateOfBirth}>
+        </div>
+        <div>
+            5] ADDRESS <br> <input type="text" id="address" value=${address}>
+        </div>
+           
+        `
+
+
+
+    
+
     // Perform any additional processing or validation here
 }
 
